98fcf93afa94829cc396d16c4223113a500e8f98
Edwinngera <edwinngera@gmail.com>
2023-01-22 10:41:23 +0300

    update

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,13 +1,13 @@
-#!usr/bin/env python
-"""
-Simple helper function 
-
-"""
+#!/usr/bin/env python3
+'''
+    Simple helper function.
+'''
 
 
 def index_range(page, page_size):
-
-    start = (page-1)*page_size
-    end = page*page_size
-
+    '''
+        Returns the range of indexes for a given page.
+    '''
+    start = (page - 1) * page_size
+    end = page * page_size
     return start, end
