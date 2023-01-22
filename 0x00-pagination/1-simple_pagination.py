4b3bf4ed083c5105f7aa647d9e19bed7d49d9f54
Edwinngera <edwinngera@gmail.com>
2023-01-22 10:46:45 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-'''
-    Simple pagination.
-'''
+"""Hypermedia pagination sample.
+"""
 import csv
 import math
-from typing import List
+from typing import Dict, List, Tuple
 
 
-def index_range(page, page_size):
-    '''
-        Returns the range of indexes for a given page.
-    '''
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
     start = (page - 1) * page_size
-    end = page * page_size
-    return start, end
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -22,9 +20,11 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
-    def dataset(self) -> List[List]:  # sourcery skip: identity-comprehension
+    def dataset(self) -> List[List]:
         """Cached dataset
         """
         if self.__dataset is None:
@@ -36,15 +36,28 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''
-            Returns a page of data.
-        '''
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        self.dataset()
-
-        if self.dataset() is None:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
+        return data[start:end]
 
-        indexRange = index_range(page, page_size)
-        return self.dataset()[indexRange[0]:indexRange[1]]
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        page_info = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
+        }
+        return page_info
