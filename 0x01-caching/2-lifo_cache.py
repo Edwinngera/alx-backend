826afc48ff8e2004a4c0423088ca5f269ad0145e
Edwinngera <edwinngera@gmail.com>
2023-01-24 21:53:13 +0300

    caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Most Recently Used caching module.
+"""Last-In First-Out caching module.
 """
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
-class MRUCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """Represents an object that allows storing and
-    retrieving items from a dictionary with an MRU
+    retrieving items from a dictionary with a LIFO
     removal mechanism when the limit is reached.
     """
     def __init__(self):
@@ -24,16 +24,12 @@ class MRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                mru_key, _ = self.cache_data.popitem(False)
-                print("DISCARD:", mru_key)
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
-        else:
-            self.cache_data[key] = item
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
         """Retrieves an item by key.
         """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Most Recently Used caching module.
+"""Least Recently Used caching module.
 """
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
-class MRUCache(BaseCaching):
+class LRUCache(BaseCaching):
     """Represents an object that allows storing and
-    retrieving items from a dictionary with an MRU
+    retrieving items from a dictionary with a LRU
     removal mechanism when the limit is reached.
     """
     def __init__(self):
@@ -24,8 +24,8 @@ class MRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                mru_key, _ = self.cache_data.popitem(False)
-                print("DISCARD:", mru_key)
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
